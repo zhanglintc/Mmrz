21affd2edeca3a58741732f699d8bf6246c8a651
Lane <zhanglintc623@gmail.com>
2016-01-06 23:53:08 +0800

    bug fix

--- db.rb
+++ db.rb
@@ -28,7 +28,7 @@ class MmrzDBManager
   end
 
   def updateDB row
-    @db.execute "update UNMMRZ set memTimes = #{row[2]}, remindTime = #{row[3]}, remindTimeStr = #{row[4]} where word = '#{row[0]}'"
+    @db.execute "update UNMMRZ set memTimes = #{row[2]}, remindTime = #{row[3]}, remindTimeStr = '#{row[4]}' where word = '#{row[0]}'"
   end
 
   def deleteDB wordID
