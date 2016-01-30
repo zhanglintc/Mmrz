a867bd13d003c294c39dd207e5542c12966492a8
Lane <zhanglintc623@gmail.com>
2016-01-30 17:08:15 +0800

    prompt

--- autoenv.rb
+++ autoenv.rb
@@ -1,14 +1,14 @@
 #!/env/bin/ruby
 # encoding: utf-8
 
-puts "Changing remote address:"
+puts "Step1: change download server"
 system "gem sources --add https://ruby.taobao.org/ --remove https://rubygems.org/"
 
 puts ""
-puts "Installing sqlite3:"
+puts "Step2: install sqlite3"
 system "gem install sqlite3"
 
 puts ""
-puts "Everything's OK"
+puts "Result: environment for Mmrz is OK"
 puts "Press any key to close..."
 gets
