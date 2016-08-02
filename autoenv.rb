fa5c31031947e3f50af2cb4a3714d58610c0217e
zhanglintc <zhanglintc623@gmail.com>
2016-08-02 11:00:17 +0800

    autoenv.rb

--- autoenv.rb
+++ autoenv.rb
@@ -1,32 +1,16 @@
 #!/env/bin/ruby
 # encoding: utf-8
 
-BAT_STR = '
-@echo off
+puts "Step1: change download server"
+system "gem sources --remove https://rubygems.org/"
+system "gem sources --remove https://ruby.taobao.org/"
+system "gem sources --add http://gems.ruby-china.org/"
 
-set SSL_CERT_FILE=%cd%\cacert.pem
+puts ""
+puts "Step2: install sqlite3"
+system "gem install sqlite3"
 
-echo 1. change download server:
-cmd /c gem sources --add https://ruby.taobao.org/ --remove https://rubygems.org/
-
-echo=
-echo 2. install sqlite3:
-cmd /c gem install sqlite3
-
-echo=
-echo Result: environment for Mmrz is OK
-echo Please close this window...
-pause>nul
-
-exit
-'
-
-CMD_FILE = "autoenv.cmd"
-
-fw = open CMD_FILE, "wb"
-fw.write BAT_STR
-fw.close
-
-puts "Don't close me before \"autoenv.cmd\" completed!!!"
-`start #{CMD_FILE}`
-`del #{CMD_FILE}`
+puts ""
+puts "Result: environment for Mmrz is OK"
+puts "Press any key to close..."
+gets
